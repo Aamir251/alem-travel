1c3ca0f8d4523fde8f438002d749d8f3b6df7d62
Aamir251 <ecodeshub@gmail.com>
2024-11-16 18:36:02 +0530

    hero poster added

--- app/_components/Hero/index.jsx
+++ app/_components/Hero/index.jsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
+
 import Gradient from "./Gradient";
-import HeroImage from "@/assets/home-hero.jpg";
+
 import Buttons from "./Buttons";
 import Container from "@/components/ui/Container";
 
@@ -8,21 +8,27 @@ import Container from "@/components/ui/Container";
 export default function Hero() {
   return (
     <section className="relative w-full h-[70vh] lg:h-[80vh] flex-center flex-col text-light text-center">
-      <Gradient />
-      <Gradient />
+      <Gradient/>
+      <Gradient/>
       
-      <video src="/hero-video.mp4" muted autoPlay loop className="absolute inset-0 h-full w-full object-cover -z-20"></video>
-      {/* <Image src={HeroImage}  
-        placeholder="blur"
-        alt="alem travels" 
-        layout="fill" style={{ objectFit: "cover" }} className="-z-20" 
+      <video autoPlay muted loop playsInline
+        className="absolute inset-0 h-full w-full object-cover -z-20"
+        poster={"/hero-poster.png"}
+      >
+        <source src="/hero-video.mp4" type="video/mp4"/>
+        Your browser does not support the video tag.
+      </video>
+      {/* <Image src={HeroImage}
+      placeholder="blur"
+      alt="alem travels"
+      layout="fill" style={{ objectFit: "cover" }} className="-z-20" F
       /> */}
-
+      
       <h1 className="text-5xl lg:text-7xl xl:text-8xl">Alem Travels</h1>
       <p className="lg:text-lg xl:text-xl">A journey of a thousand miles begins with a single step</p>
       <Container className={`flex justify-center`}>
-
-        <Buttons />
+        
+        <Buttons/>
       </Container>
     </section>
   )
